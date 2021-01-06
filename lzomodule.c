1a0f735807b6ebf3e4ed86808dc561ac6807cdba
horeqasupplies <61612727+horeqasupplies@users.noreply.github.com>
2021-01-06 23:57:24 +0530

    changes

--- lzomodule.c
+++ lzomodule.c
@@ -192,7 +192,7 @@ decompress(PyObject *dummy, PyObject *args)
     lzo_uint new_len;
     int len;
     int buflen = -1;
-    int header = 0;
+    int header = 1;
     int err;
 
     /* init */
@@ -211,7 +211,7 @@ decompress(PyObject *dummy, PyObject *args)
     else {
         if (buflen < 0) return PyErr_Format(LzoError, "Argument buflen required for headerless decompression");
         out_len = buflen;
-        in_len = buflen-4;
+        in_len = buflen - 4;
     }
 
     /* alloc buffers */
@@ -231,9 +231,10 @@ decompress(PyObject *dummy, PyObject *args)
 #endif
 
     Py_BEGIN_ALLOW_THREADS
-    new_len = 0;
+    new_len = out_len;
     err = lzo1z_decompress(in, in_len, out, &new_len, NULL);
     Py_END_ALLOW_THREADS
+    return result_str;
     if (err != LZO_E_OK || (header && new_len != out_len) )
     {
         Py_DECREF(result_str);
